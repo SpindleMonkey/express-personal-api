503f1081449a594383951a8548d88506fa1d28b9
connie kephart <spindle.monkey@gmail.com>
2017-07-26 18:43:48 -0600

    adds routes for travel api

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 mongoose.connect( process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/personal-api");
+                  'mongodb://localhost/personal-api');
 
-// module.exports.Campsite = require("./campsite.js.example");
+module.exports.Travel = require('./travel.js');
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // require express and other modules
-var express = require('express'),
-    app = express();
+var express = require('express');
+var app = express();
 
 // parse incoming urlencoded form data
 // and populate the req.body object
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -39,15 +40,80 @@ app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    message: 'Welcome to my personal api! Here\'s what you need to know:',
+    documentation_url: 'https://github.com/SpindleMonkey/express-personal-api/blob/master/README.md',
+    base_url: 'https://enigmatic-mountain-86685.herokuapp.com/',
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: 'GET', path: '/api', description: 'Describes all available endpoints'},
+      {method: 'GET', path: '/api/profile', description: 'Data about me'}, 
+      {method: 'GET', path: '/api/travel', description: 'My travel wish list'},
+      {method: 'GET', path: '/api/travel/:where', description: 'Show any destinations matching \'where\' in my travel wish list'},
+      {method: 'POST', path: '/api/travel', description: 'Add another destination to my travel wish list'},
+      {method: 'PUT', path: '/api/travel/:id', description: 'Update one of my travel destinations'},
+      {method: 'DELETE', path: '/api/travel/:id', description: 'Remove one of my travel destinations'},
     ]
-  })
+  });
+});
+
+app.get('/api/profile', function apiProfile(req, res) {
+  res.json({
+    name: 'Connie Kephart',
+    github_link: 'https://github.com/SpindleMonkey',
+    github_profile_image: 'https://avatars2.githubusercontent.com/u/3019766?v=4&u=bb613c37678dc30dc8d010d3d6b6dd1c762c7fc5&s=400',
+    current_city: 'Coal Creek Canyon in unincorporated Jefferson County',
+    pets: [
+      {name: 'Winterfyre Anastasia', nickname: 'Annie', age: 16, gender: 'female', type: 'cat', breed: 'Norwegian Forest Cat'},
+      {name: 'Winterfyre Winterfyre Oskar Sylvanus', nickname: 'Oskar', age: 13, gender: 'male', type: 'cat', breed: 'Norwegian Forest Cat'},
+      {name: 'Winterfyre Zelda Gypsydottir', nickname: 'Zelda', age: 12, gender: 'female', type: 'cat', breed: 'Norwegian Forest Cat'},
+      {name: 'Lost Woods Galia', nickname: 'Ollie', age: 9, gender: 'female', type: 'cat', breed: 'Norwegian Forest Cat'},
+    ],
+  });
+});
+
+app.get('/api/travel', function apiTravelIndex(req, res) {
+  db.Travel.find({}, function(err, travels) {
+    if (err) { return console.log("ERROR::" + err); }
+    res.json({travels: travels});
+  });
+});
+
+app.get('/api/travel/:where', function apiTravelShow(req, res) {
+  db.Travel.find({where: req.params.where}, function(err, travels) {
+    if (err) { return console.log("ERROR::" + err); }
+    res.json({travels: travels});
+  });
+});
+
+app.post('/api/travel', function apiTravelNew(req, res) {
+  console.log(req.body);
+  // check the new destination: must have where and why; when and image are optional
+  if (req.body.where && req.body.why) {
+    var newTravel = db.Travel({
+      where: req.body.where,
+      why: req.body.why,
+      when: req.body.when,
+      image: req.body.image
+    });
+    newTravel.save(function(err, travel) {
+      if (err) { res.send('ERROR::' + err); }
+      res.json(travel);
+    });
+  }
+});
+
+app.put('/api/travel/:where', function apiTravelUpdate(req, res) {
+  console.log(req.params.where);
+  console.log(req.body);
+  db.Travel.findOneAndUpdate({where: req.params.where}, function(err, travel) {
+
+  });
+});
+
+app.delete('/api/travel/:where', function apiTravelRemove(req, res) {
+  db.Travel.remove({where: req.params.where}, function(err, travel) {
+    if (err) { res.status(404).send('ERROR::' + err); }
+    res.send(req.params.where + ' has been removed');
+  });
 });
 
 /**********
